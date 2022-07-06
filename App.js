1913e7dfefffb3ba556bcb4930a44bae0ecaca1a
meerkatstudio <patrick.lemmer@yahoo.com>
2022-07-06 10:39:35 +0200

    installed and imported dependencies

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+// Gesture handler import (purposly at the top)
+import 'react-native-gesture-handler';
+
+// React imports
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import { StatusBar } from 'expo-status-bar';
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Chat App</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+        <Text>Chat App</Text>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
