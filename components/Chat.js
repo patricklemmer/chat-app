b7bc611a49883a4b17db251b16f7eef5c86b24ae
meerkatstudio <patrick.lemmer@yahoo.com>
2022-07-06 11:02:55 +0200

    added message state and GiftedChat component

--- components/Chat.js
+++ components/Chat.js
@@ -1,12 +1,42 @@
 // React imports
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    // State initialization
+    this.state = {
+      messages: [],
+    };
+  }
+
   componentDidMount() {
     // Set name as chat title
     let { name } = this.props.route.params;
     this.props.navigation.setOptions({ title: name });
+
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }));
   }
 
   render() {
@@ -16,6 +46,16 @@ export default class Chat extends React.Component {
       color = '#B9C6AE';
     }
 
-    return <View style={[{ backgroundColor: color }, { flex: 1 }]}></View>;
+    return (
+      <View style={[{ backgroundColor: color }, { flex: 1 }]}>
+        <GiftedChat
+          messages={this.state.messages}
+          onSend={(messages) => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+      </View>
+    );
   }
 }
